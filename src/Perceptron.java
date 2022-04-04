17a5b74d30031c7d0e3af687949289686e75dd39
Leonardo Borges <leonardo3ktt@gmail.com>
2022-04-04 14:47:11 -0300

    Aprimorando a exibição no console

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,104 +1,217 @@
+import java.util.Scanner;
+
 public class Perceptron {
 
 	public static void main(String[] args) {
 
-		int alpha = 1;
-		int threshold = 1;
+		Scanner sc = new Scanner(System.in);
 
-		int fYent = 0;
-		int generation = 0;
-		String operation = "and";
-		int[] WEIGHT = { 0, 0, 0, 0 };
-		boolean hasWeightVariation = true;
+		boolean rodarNovamente = true;
 
-		int[][] TRUTH_TABLE = { { 1, 1, 1, 1 }, { 1, 1, -1, 1 }, { 1, -1, 1, 1 }, { 1, -1, -1, 1 }, { -1, 1, 1, 1 },
-				{ -1, 1, -1, 1 }, { -1, -1, 1, 1 }, { -1, -1, -1, 1 } };
+		while (rodarNovamente) {
 
-		int[] targetAnd = { 1, -1, -1, -1, -1, -1, -1, -1 };
-		int[] targetOr = { 1, 1, 1, 1, 1, 1, 1, -1 };
-		int[] targetXor = { 0, 1, 1, 0, 1, 0, 0, 0 };
+			int alpha = 1;
+			int limite = 1;
 
-		int[] TARGET = targetAnd;
+			int fYent = 0;
+			int geracao = 0;
+			int[] pesos = { 0, 0, 0, 0 };
+			boolean variaPeso = true;
 
-		while (hasWeightVariation) {
-			int[] allYent = {};
-			int[] allFYent = {};
-			int[] allWeights = {};
-			int[][] weightVariation = { {}, {}, {}, {}, {}, {}, {}, {} };
+			int[][] tabelaVerdade = { { 1, 1, 1, 1 }, { 1, 1, -1, 1 }, { 1, -1, 1, 1 }, { 1, -1, -1, 1 },
+					{ -1, 1, 1, 1 }, { -1, 1, -1, 1 }, { -1, -1, 1, 1 }, { -1, -1, -1, 1 } };
 
-			generation += 1;
+			int[] targetAnd = { 1, -1, -1, -1, -1, -1, -1, -1 };
+			int[] targetOr = { 1, 1, 1, 1, 1, 1, 1, -1 };
+			int[] targetXor = { 0, 1, 1, 0, 1, 0, 0, 0 };
 
-			for (int line = 0; line < 8; line++) {
-				int yent = 0;
+			int[] target = new int[8];
+			int[] todosPesos = new int[8];;
+			String operation;
+			
+			boolean operacaoValida = false;
+			System.out.println();
+			System.out.print("------------------ PERCEPTRON ------------------");
+			System.out.println();
 
-				// Cálculo do Yent
-				for (int column = 0; column < 4; column++) {
-					yent += TRUTH_TABLE[line][column] * WEIGHT[column];
-				}
-
-				// Captura do (f)Yent
-				fYent = getFYent(yent, threshold);
-
-				int wVariationFirstPart = alpha * (TARGET[line] - fYent);
-
-				// Mapeamento das variações dos pesos (matriz(3,4))
-				weightVariation[line][0] = wVariationFirstPart * TRUTH_TABLE[line][0];
-				weightVariation[line][1] = wVariationFirstPart * TRUTH_TABLE[line][1];
-				weightVariation[line][2] = wVariationFirstPart * TRUTH_TABLE[line][2];
-				weightVariation[line][3] = wVariationFirstPart * TRUTH_TABLE[line][3];
-
-				// Atualização dos valores dos pesos
-				WEIGHT[0] += weightVariation[line][0];
-				WEIGHT[1] += weightVariation[line][1];
-				WEIGHT[2] += weightVariation[line][2];
-				WEIGHT[3] += weightVariation[line][3];
-
-				System.out.println("Epoca: " + generation);
-				System.out.println("Pesos: ");
-				System.out.println(WEIGHT);
-				System.out.println("Yent: ");
-				System.out.println(yent);
-				System.out.println("FYent: ");
-				System.out.println(fYent);
+			do {
+				System.out.println();
+				System.out.print("Informe a Operação: ");
+				operation = sc.nextLine();
 				System.out.println();
 
-//			      allYent.push(yent);
-//			      allFYent.push(fYent);
-//			      allWeights.push(WEIGHT);
+				if (operation.equalsIgnoreCase("AND")) {
+					target = targetAnd;
+					operacaoValida = true;
+
+				} else if (operation.equalsIgnoreCase("OR")) {
+					target = targetOr;
+					operacaoValida = true;
+
+				} else if (operation.equalsIgnoreCase("XOR")) {
+					target = targetXor;
+					operacaoValida = true;
+				}
+				else {
+					System.out.print("Operação Inválida! Tente Novamente.");
+				}
+			} while(!operacaoValida);
+
+
+			while (variaPeso) {
+				int[] allYent = new int[8];
+				int[] allFYent = new int[8];
+				
+				int[][] variacaoPeso = { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+
+				geracao += 1;
+
+				for (int linha = 0; linha < 8; linha++) {
+					int yent = 0;
+
+					for (int coluna = 0; coluna < 4; coluna++) {
+						yent += tabelaVerdade[linha][coluna] * pesos[coluna];
+					}
+
+					if (yent > limite) {
+						fYent = 1;
+					} else if (yent <= limite && yent >= -limite) {
+						fYent = 0;
+					} else {
+						fYent = -1;
+					}
+
+					int variacao = alpha * (target[linha] - fYent);
+
+					variacaoPeso[linha][0] = variacao * tabelaVerdade[linha][0];
+					variacaoPeso[linha][1] = variacao * tabelaVerdade[linha][1];
+					variacaoPeso[linha][2] = variacao * tabelaVerdade[linha][2];
+					variacaoPeso[linha][3] = variacao * tabelaVerdade[linha][3];
+
+					pesos[0] += variacaoPeso[linha][0];
+					pesos[1] += variacaoPeso[linha][1];
+					pesos[2] += variacaoPeso[linha][2];
+					pesos[3] += variacaoPeso[linha][3];
+
+					allYent[linha] = yent;
+					allFYent[linha] = fYent;
+					todosPesos = pesos;
+				}
+
+				System.out.println("Epoca: " + geracao);
+				System.out.println();
+				System.out.println();
+				int count = 1;
+				System.out.print("Yent: [");
+				count = 1;
+				for (int Yent : allYent) {
+					if (count == 8) {
+						System.out.print(Yent);
+					}
+					else {
+						System.out.print(Yent + ", ");
+					}
+					count++;
+				}
+				System.out.print("] ");
+				System.out.println();
+				System.out.println();
+				System.out.print("FYent: [");
+				count = 1;
+				for (int FYent : allFYent) {
+					if (count == 8) {
+						System.out.print(FYent);
+					} else {
+						System.out.print(FYent + ", ");
+					}
+					count++;
+				}
+				System.out.print("] ");
+				System.out.println();
+				System.out.println();
+				System.out.print("Pesos: ");
+				System.out.print("[");
+				count = 1;
+				for (int peso : todosPesos) {
+					if (count == 4) {
+						System.out.print(peso);
+					} else {
+						System.out.print(peso + ", ");
+					}
+					count++;
+				}
+				System.out.print("] ");
+				System.out.println();
+				System.out.println("------------------------------------------------");
+
+				boolean achouResultado = pesoVaria(variacaoPeso);
+
+				if (geracao == 50 && operation.equalsIgnoreCase("XOR")) {
+					variaPeso = false;
+					System.out.println("");
+					System.out.println("Utilizando a operação XOR o looping se tornou infinito!");
+				}
+
+				if (achouResultado) {
+					variaPeso = false;
+				}
 			}
 
-			boolean done = noWeightVariation(weightVariation);
-
-			if (generation == 100 && operation == "xor") {
-				hasWeightVariation = false;
-				System.out.println(
-						"A operação XOR é um problema não linear, por isso esse algoritmo não consegue resolver. Foi limitado em 100 épocas para demostração!");
-				continue;
+			boolean opcaoValida = false;
+			
+			if(!operation.equalsIgnoreCase("XOR")) {
+				System.out.println("");
+				System.out.print("Pesos Finais: ");
+				System.out.print("[");
+				int count = 1;
+				for (int peso : todosPesos) {
+					if (count == 4) {
+						System.out.print(peso);
+					} else {
+						System.out.print(peso + ", ");
+					}
+					count++;
+				}
+				System.out.print("] ");
+				System.out.println();
 			}
 
-			if (done) {
-				hasWeightVariation = false;
-				continue;
-			}
+			System.out.println();
+			System.out.println("Épocas Geradas: " + geracao);
+			System.out.println();
+			System.out.println("------------------------------------------------");
+
+			do {
+				System.out.println();
+				System.out.print("Deseja Rodar Novamente(S / N)? ");
+				String rodar = sc.nextLine();
+				System.out.println();
+
+				if (rodar.equalsIgnoreCase("S") || rodar.equalsIgnoreCase("N")) {
+					opcaoValida = true;
+				}
+				else {
+					System.out.println();
+					System.out.println("Erro! Informe uma opção válida (S / N).");
+					System.out.println();
+				}
+
+				if (rodar.equalsIgnoreCase("N")) {
+					rodarNovamente = false;
+				}
+				
+			} while (!opcaoValida);
+
 		}
-
+		sc.close();
 	}
 
-	public static int getFYent(int yent, int threshold) {
-		if (yent > threshold) {
-			return 1;
-		} else if (yent <= threshold && yent >= -threshold) {
-			return 0;
-		} else {
-			return -1;
-		}
-	}
-
-	public static boolean noWeightVariation(int[][] weightVariation) {
+	public static boolean pesoVaria(int[][] variacaoPeso) {
 		boolean done = true;
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 4; j++) {
-				if (weightVariation[i][j] != 0) {
+				if (variacaoPeso[i][j] != 0) {
 					done = false;
 					break;
 				}
